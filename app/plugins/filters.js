d9639bef55f8d7dec912e0a4a50fad80ced0272f
Abu Uzayr <abu.uzayr@builtforfifty.com>
2018-01-16 21:56:53 +0800

    Complete user management tables - reduce font size - add styling for user mgmt components - set table headers for user mgmt tables - Add tooltip for use

--- app/plugins/filters.js
+++ app/plugins/filters.js
@@ -1,5 +1,24 @@
 import Vue from 'vue';
 
+function dateFormat(value) {
+  if (!value) return '';
+
+  let date = twoDigits(value.getDate()) + '/';
+  date = date + twoDigits(value.getMonth() + 1) + '/';
+  date = date + value.getFullYear();
+
+  return date;
+}
+
+function numFormat(value) {
+  if (!value) return '';
+  value = value.toString();
+  value = value.replace(new RegExp('^(\\d{' + (value.length%3? value.length%3:0) + '})(\\d{3})', 'g'), '$1 $2').replace(/(\d{3})+?/gi, '$1 ').trim();
+  value = value.replace(/\s/g, ',');
+
+  return value;
+}
+
 function twoDigits(value) {
   if (value.toString().length <= 1) {
     return `0${value.toString()}`;
@@ -8,4 +27,6 @@ function twoDigits(value) {
   return value.toString();
 }
 
-Vue.filter('twoDigits', twoDigits);
+Vue.filter('numFormat', numFormat);
+Vue.filter("twoDigits", twoDigits);
+Vue.filter('dateFormat', dateFormat);
